ab5f2e75103e7e97e5f3e9fdbbce9d0198abacef
lsebastian1991@gmail.com <seu.email@example.com>
2025-01-02 10:02:20 +0000

    Add icons to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,13 +7,20 @@ import SoundCloud from "../../assets/Icons/SVG/soundcloud.svg";
 import Spotify from "../../assets/Icons/SVG/spotify.svg";
 import Youtube from "../../assets/Icons/SVG/youtube.svg";
 
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
 export default function Footer() {
   return (
     <>
       <footer className="footer">
         <nav className="footer__nav">
           <div className="footer__title-wrap">
-            <Link to="/contact" className="footer__logo">
+            <Link to="/contact" className="footer__logo" onClick={scrollToTop}>
               <h2 className="footer__title">Get in Touch</h2>
             </Link>
           </div>
