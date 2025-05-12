525e65096f6d25fd4cbde6e2337ec3bda4e5be6b
lsebastian1991@gmail.com <139809146+Lucasnistico@users.noreply.github.com>
2025-05-12 20:52:38 +0000

    fix some issues

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -14,55 +14,58 @@ export default function About() {
 
   return (
     <>
-      <section className="about__wrap">
-        <img src={Logo} alt="Gui Tavares" className="about__logo" />
-        <div className="about__title-wrap">
-          <p className="about__subtitle">
-            Samba é a voz do meu país, está em tudo o que se diz. O mundo
-            precisa te encontrar! Canta, solta a voz para se ouvir, deixa o céu
-            iluminar, salva o samba que chegar.&#127925;&#127926;
-          </p>
-          <p className="about__subtitle about__subtitle--slogan">
-            Song Writter | Singer | Multi intrumentalist | Conductor
-          </p>
-          <p className="about__subtitle about__subtitle--slogan">
-            Gui Tavares: One of the faces of Brazilian music in the UK to the
-            world!{" "}
-          </p>
-        </div>
-        <div className="about__records">
-          <a
-            href="https://soundcloud.com/guitavares-127051646"
-            className="about__records-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Records />
-          </a>
-        </div>
-        <div className="about__contents">
-          <div className="about__content about__content--top">
-            Graduated in Classical and Popular Guitar, as well as Composition
-            and Conducting from UNICAMP, Gui Tavares is a distinct voice in
-            contemporary Brazilian music. His first independent album, Noite que
-            Brincou de Lua, released in partnership with Edu Passeto, marked the
-            beginning of his recording career and was later released by Far Out
-            Records due to the cultural impact it achieved.
+      <div className="about">
+        <section className="about__wrap">
+          <img src={Logo} alt="Gui Tavares" className="about__logo" />
+          <div className="about__title-wrap">
+            <p className="about__subtitle">
+              Samba é a voz do meu país, está em tudo o que se diz. O mundo
+              precisa te encontrar! Canta, solta a voz para se ouvir, deixa o
+              céu iluminar, salva o samba que chegar.&#127925;&#127926;
+            </p>
+            <p className="about__subtitle about__subtitle--slogan">
+              Song Writter | Singer | Multi intrumentalist | Conductor
+            </p>
+            <p className="about__subtitle about__subtitle--slogan">
+              Gui Tavares: One of the faces of Brazilian music in the UK to the
+              world!{" "}
+            </p>
           </div>
-          <div className="about__content">
-            His career includes collaborations on albums by artists such as
-            Jazzinho and Ed Motta, as well as contributions to two kids music
-            compilations from the American label Putumayo Records. Gui leads
-            projects that celebrate Brazilian music, such as Clube da Esquina
-            Tribute, Minas Heart of Brazil, and the vocal group Nossa Voz, which
-            has over 20 years. Additionally, he promotes lectures and workshops
-            on Brazil's musical culture.
+          <div className="about__records">
+            <a
+              href="https://soundcloud.com/guitavares-127051646"
+              className="about__records-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Records />
+            </a>
           </div>
-        </div>
-        <Link to="/bookings" className="about__btn" onClick={scrollToTop}>
-          Book Now!
-        </Link>
-      </section>
+          <div className="about__contents">
+            <div className="about__content about__content--top">
+              Graduated in Classical and Popular Guitar, as well as Composition
+              and Conducting from UNICAMP, Gui Tavares is a distinct voice in
+              contemporary Brazilian music. His first independent album, Noite
+              que Brincou de Lua, released in partnership with Edu Passeto,
+              marked the beginning of his recording career and was later
+              released by Far Out Records due to the cultural impact it
+              achieved.
+            </div>
+            <div className="about__content">
+              His career includes collaborations on albums by artists such as
+              Jazzinho and Ed Motta, as well as contributions to two kids music
+              compilations from the American label Putumayo Records. Gui leads
+              projects that celebrate Brazilian music, such as Clube da Esquina
+              Tribute, Minas Heart of Brazil, and the vocal group Nossa Voz,
+              which has over 20 years. Additionally, he promotes lectures and
+              workshops on Brazil's musical culture.
+            </div>
+          </div>
+          <Link to="/bookings" className="about__btn" onClick={scrollToTop}>
+            Book Now!
+          </Link>
+        </section>
+      </div>
     </>
   );
 }
